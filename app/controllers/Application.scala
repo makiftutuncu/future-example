43ae01e5318c0f9776cd4b8ac6c7b4df61caa834
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-01-29 17:02:27 +0200

    Used Iteratee for downloading image and improved UI with Bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,26 +10,43 @@ import play.api.data.Forms._
 import play.api.libs.ws.WS
 import play.api.libs.iteratee._
 
+/**
+ * A tiny and utterly useless class for retrieving URL provided by the user
+ */
 case class DownloadUrl(url: String)
 
 object Application extends Controller
 {
+  /**
+   * A simple URL validation method for image links
+   */
   def isValidUrl(url: String) =
   {
-    ((url startsWith "http://") || (url startsWith "https://")) && (
-    (url endsWith ".jpg") || (url endsWith ".jpeg") || (url endsWith ".JPG") || (url endsWith ".JPEG") ||
-    (url endsWith ".png") || (url endsWith ".PNG") ||
-    (url endsWith ".gif") || (url endsWith ".GIF"))
+    ((url startsWith "http://") || (url startsWith "https://")) &&
+    ((url endsWith ".jpg") || (url endsWith ".jpeg") || (url endsWith ".JPG") || (url endsWith ".JPEG"))
   }
 
+  /**
+   * A Play Form for retrieving the URL provided by the user
+   */
   val downloadForm: Form[DownloadUrl] = Form(
     mapping("url" -> nonEmptyText.verifying("Invalid image URL!", isValidUrl(_)))
     (DownloadUrl.apply)(DownloadUrl.unapply)
   )
 
-  val path: String = """public/images/"""
-  val fileName: String = """image.jpg"""
+  /**
+   * Path to which the image will be downloaded
+   */
+  private val path: String = """public/images/"""
 
+  /**
+   * Name of the file to save the image
+   */
+  private val fileName: String = """image.jpg"""
+
+  /**
+   * Main action for rendering the page
+   */
   def index = Action {
     val file = new File(path + fileName)
     if(file.exists)
@@ -38,12 +55,14 @@ object Application extends Controller
       Ok(views.html.index(""))
   }
 
-  def fromStream(stream: OutputStream): Iteratee[Array[Byte], Unit] = Cont {
-    case e @ Input.EOF =>
+  /**
+   * Iteratee method to process stream
+   */
+  def fromStream(stream: OutputStream): Iteratee[Array[Byte], SimpleResult] = Cont {
+    case Input.EOF =>
       stream.close()
       Logger.debug("Successful!")
-      Redirect(routes.Application.index)
-      Done((), e)
+      Done(Redirect(routes.Application.index()))
 
     case Input.El(data) =>
       stream.write(data)
@@ -51,38 +70,36 @@ object Application extends Controller
       fromStream(stream)
 
     case Input.Empty =>
+      Logger.debug("Received empty input.")
       fromStream(stream)
   }
 
+  /**
+   * Download action that reads the URL from user input and runs downloading task
+   */
   def download = Action.async {
     implicit request => downloadForm.bindFromRequest.fold(
       errors => {
         Logger.error("Cannot download: " + errors.errorsAsJson)
         Future.successful {
-          Redirect(routes.Application.index)
+          Redirect(routes.Application.index())
         }
       },
       downloadUrl => {
         Logger.debug("Starting to download...")
-        WS.url(downloadUrl.url).withRequestTimeout(10000).get map {
-        response =>
-            try
-            {
-              val out = new FileOutputStream(path + fileName)
-              out.write(response.getAHCResponse.getResponseBodyAsBytes)
-              out.close()
-              Logger.debug("Successful!")
-              Redirect(routes.Application.index)
-            }
-            catch
-            {
-              case _ => throw new Exception("IO exception!")
-            }
+
+        // Start a GET request with 10 seconds timeout
+        WS.url(downloadUrl.url).withRequestTimeout(10000).get {
+          // Write the request to a file
+          headers => fromStream(new BufferedOutputStream(new FileOutputStream(path + fileName)))
+        } flatMap {
+          // Run the iteration for downloading process
+          _.run
         } recover {
-          case _ =>
-            Logger.debug("Failed!")
+          case e: Exception =>
+            Logger.debug("Failed! " + e.getMessage)
             new File(path + fileName).delete()
-            Redirect(routes.Application.index)
+            Redirect(routes.Application.index())
         }
       }
     )
