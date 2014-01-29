12e449209a75e3b5fb8f84a6956b5937aa808c30
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-01-29 16:04:35 +0200

    Download operation is non-blocking
    TODO: Make file IO non-blocking as well

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,13 +3,12 @@ package controllers
 import play.api.mvc._
 import scala.concurrent._
 import ExecutionContext.Implicits.global
-import scala.util._
-import sys.process._
-import java.net.URL
-import java.io.File
+import java.io._
 import play.Logger
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.ws.WS
+import play.api.libs.iteratee._
 
 case class DownloadUrl(url: String)
 
@@ -18,14 +17,9 @@ object Application extends Controller
   def isValidUrl(url: String) =
   {
     ((url startsWith "http://") || (url startsWith "https://")) && (
-    (url endsWith ".jpg") ||
-    (url endsWith ".jpeg") ||
-    (url endsWith ".JPG") ||
-    (url endsWith ".JPEG") ||
-    (url endsWith ".png") ||
-    (url endsWith ".PNG") ||
-    (url endsWith ".gif") ||
-    (url endsWith ".GIF"))
+    (url endsWith ".jpg") || (url endsWith ".jpeg") || (url endsWith ".JPG") || (url endsWith ".JPEG") ||
+    (url endsWith ".png") || (url endsWith ".PNG") ||
+    (url endsWith ".gif") || (url endsWith ".GIF"))
   }
 
   val downloadForm: Form[DownloadUrl] = Form(
@@ -33,19 +27,34 @@ object Application extends Controller
     (DownloadUrl.apply)(DownloadUrl.unapply)
   )
 
+  val path: String = """public/images/"""
   val fileName: String = """image.jpg"""
 
-  def index = Action
-  {
-    val file = new File("public/images/" + fileName)
+  def index = Action {
+    val file = new File(path + fileName)
     if(file.exists)
       Ok(views.html.index(file.getName))
     else
       Ok(views.html.index(""))
   }
 
-  def download = Action.async
-  {
+  def fromStream(stream: OutputStream): Iteratee[Array[Byte], Unit] = Cont {
+    case e @ Input.EOF =>
+      stream.close()
+      Logger.debug("Successful!")
+      Redirect(routes.Application.index)
+      Done((), e)
+
+    case Input.El(data) =>
+      stream.write(data)
+      Logger.debug(s"Downloaded ${data.length} bytes.")
+      fromStream(stream)
+
+    case Input.Empty =>
+      fromStream(stream)
+  }
+
+  def download = Action.async {
     implicit request => downloadForm.bindFromRequest.fold(
       errors => {
         Logger.error("Cannot download: " + errors.errorsAsJson)
@@ -54,28 +63,26 @@ object Application extends Controller
         }
       },
       downloadUrl => {
-        val f = Future
-        {
-          Logger.debug("Starting to download...")
-
-          val url = new URL(downloadUrl.url)
-          val file = new File("public/images/" + fileName)
-          url #> file !!
-        }
-
-        f map {
-          result => {
-            Logger.debug("Successful!")
-            Redirect(routes.Application.index())
-          }
-        } recover {
-          case _ => {
-            Logger.debug("Failed!")
-            Future {
-              new File("public/images/" + fileName).delete()
+        Logger.debug("Starting to download...")
+        WS.url(downloadUrl.url).withRequestTimeout(10000).get map {
+        response =>
+            try
+            {
+              val out = new FileOutputStream(path + fileName)
+              out.write(response.getAHCResponse.getResponseBodyAsBytes)
+              out.close()
+              Logger.debug("Successful!")
+              Redirect(routes.Application.index)
             }
-            Redirect(routes.Application.index())
-          }
+            catch
+            {
+              case _ => throw new Exception("IO exception!")
+            }
+        } recover {
+          case _ =>
+            Logger.debug("Failed!")
+            new File(path + fileName).delete()
+            Redirect(routes.Application.index)
         }
       }
     )
