4a872daac278831430d6ebfd552f13c5ae40f04d
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2014-01-29 12:26:22 +0200

    Download url is read from user from, downloaded image can be shown

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,31 +8,76 @@ import sys.process._
 import java.net.URL
 import java.io.File
 import play.Logger
+import play.api.data.Form
+import play.api.data.Forms._
+
+case class DownloadUrl(url: String)
 
 object Application extends Controller
 {
-  val fileUrl: String = """https://dl.dropboxusercontent.com/u/37485576/Turkish%20Flag.jpg"""
-  val fileName: String = """flag.jpg"""
+  def isValidUrl(url: String) =
+  {
+    ((url startsWith "http://") || (url startsWith "https://")) && (
+    (url endsWith ".jpg") ||
+    (url endsWith ".jpeg") ||
+    (url endsWith ".JPG") ||
+    (url endsWith ".JPEG") ||
+    (url endsWith ".png") ||
+    (url endsWith ".PNG") ||
+    (url endsWith ".gif") ||
+    (url endsWith ".GIF"))
+  }
+
+  val downloadForm: Form[DownloadUrl] = Form(
+    mapping("url" -> nonEmptyText.verifying("Invalid image URL!", isValidUrl(_)))
+    (DownloadUrl.apply)(DownloadUrl.unapply)
+  )
+
+  val fileName: String = """image.jpg"""
 
   def index = Action
   {
-    val f = Future
-    {
-      Logger.debug("Starting to download...")
+    val file = new File("public/images/" + fileName)
+    if(file.exists)
+      Ok(views.html.index(file.getName))
+    else
+      Ok(views.html.index(""))
+  }
 
-      val url = new URL(fileUrl)
-      val file = new File("public/images/" + fileName)
-      url #> file !!
-    }
+  def download = Action.async
+  {
+    implicit request => downloadForm.bindFromRequest.fold(
+      errors => {
+        Logger.error("Cannot download: " + errors.errorsAsJson)
+        Future.successful {
+          Redirect(routes.Application.index)
+        }
+      },
+      downloadUrl => {
+        val f = Future
+        {
+          Logger.debug("Starting to download...")
 
-    f map
-    {
-      result => Logger.debug("Successful!")
-    } recover
-    {
-      case _ => Logger.debug("Failed!"); Future { new File("public/images/" + fileName).delete() }
-    }
+          val url = new URL(downloadUrl.url)
+          val file = new File("public/images/" + fileName)
+          url #> file !!
+        }
 
-    Ok("Image download with Scala Future")
+        f map {
+          result => {
+            Logger.debug("Successful!")
+            Redirect(routes.Application.index())
+          }
+        } recover {
+          case _ => {
+            Logger.debug("Failed!")
+            Future {
+              new File("public/images/" + fileName).delete()
+            }
+            Redirect(routes.Application.index())
+          }
+        }
+      }
+    )
   }
 }
